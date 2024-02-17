694d1f1d55c647a3d409511bcb14d876ca9c0e96
Pranav Mahajan <pranavmahajan18@gmail.com>
2024-02-17 20:31:21 +0530

    Create Ascii.java

---
+++ Assignment-4/Ascii.java
@@ -0,0 +1,7 @@
+public class Ascii {
+    public static void main(String[] args) {
+        for (char x = 'A'; x <= 'z'; x++) {
+            System.out.println(x + ": " + (int) x);
+        }
+    }
+}
